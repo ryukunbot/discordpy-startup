7e73af9c86cc918836bf7fb488f270ad6e005f5d
ryukun0808bot <85101267+ryukunbot@users.noreply.github.com>
2021-12-30 03:58:51 +0900

    discord.py

--- discordbot.py
+++ discordbot.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+bot = commands.Bot(command_prefix='r?')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -15,7 +15,7 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('pong')
+    await ctx.send('おはよ')
 
-
+    
 bot.run(token)
